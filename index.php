8080a2b864a654243b71c573327f542af0822c5a
dwipstyle <dwiwahyu.permata@gmail.com>
2026-01-02 10:41:57 +0700

    menambahkan list

--- index.php
+++ index.php
@@ -8,5 +8,9 @@
 <body>
     <h1>Hello Word</h1>
     <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Placeat perspiciatis, fuga recusandae possimus dicta commodi non officia maxime veritatis labore quibusdam doloribus eos laborum ea! Quos nesciunt modi cum placeat.</p>
+    <ul>
+        <li>satu</li>
+        <li>dua</li>
+    </ul>
 </body>
 </html>
